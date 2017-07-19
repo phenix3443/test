7023fc1d14fb5a2edd7a733149ac2f592f2a2948
phenix3443 <phenix3443@gmail.com>
2017-07-19 23:36:53 +0800

    add test

--- go/test/src/build.sh
+++ go/test/src/build.sh
@@ -3,3 +3,8 @@ cur_dir=$(cd $(dirname $0); pwd)
 project_dir=$(realpath $cur_dir/..)
 export GOPATH=${project_dir}
 export PATH=$PATH:$GOPATH/bin
+
+go install example.com/stringutil
+go install example.com/hello
+
+hello
---
+++ go/test/src/example.com/hello/hello.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"example.com/stringutil"
+)
+
+func main() {
+	fmt.Printf(stringutil.Reverse("!oG ,olleH"))
+}
