8e5ed9da13c37d31f691a98055933af9862d1c90
liushangliang <liushangliang@xunlei.com>
2017-07-27 21:17:57 +0800

    修复环境变量重复添加的问题

--- go/test/.dir-locals.el
+++ go/test/.dir-locals.el
@@ -4,6 +4,10 @@
 						 (if (stringp d) d (car d))))))
 		 (eval . (message "Project directory set to `%s'." project-dir))))
 
- (go-mode . ((eval . (setenv "GOPATH" (concat (getenv "GOPATH") ":" project-dir)))
+ (go-mode . ((eval . (let ((gopath (getenv "GOPATH")))
+					   (if (not (string-match-p project-dir gopath))
+						   (progn
+							 (setenv "GOPATH" (concat (getenv "GOPATH") ":" project-dir))
+							 (setenv "PATH" (concat (getenv "PATH") ":" (concat project-dir "bin")))))))
 			 ))
  )
