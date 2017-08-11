80a48cb210757c6c25a422f284e644815ff62256
liushangliang <liushangliang@xunlei.com>
2017-08-11 22:12:21 +0800

    add go test code

--- go/test/src/example.com/hello/hello.go
+++ go/test/src/example.com/hello/hello.go
@@ -1,16 +1,45 @@
 package main
 
 import (
-	// "fmt"
-	"log"
-	"runtime"
+	"fmt"
 )
 
 func main() {
-	where()
+	fmt.Println("in main f1", f1())
+	fmt.Println("in main f2", f2())
+	fmt.Println("in main f3", f3())
 }
 
-func where() {
-	_, file, line, _ := runtime.Caller(1)
-	log.Printf("%s:%d", file, line)
+// 非命名返回值
+func f1() int {
+	var a int
+	defer func(x *int) {
+		*x = 1
+	}(&a)
+	return a
+}
+
+// 命名参数，修改返回值
+func f2() (y int) {
+	var a int
+	defer func(x *int) {
+		*x = 2
+	}(&a)
+	return a
+}
+
+// 命名参数，修改命名参数
+func f3() (y int) {
+	var a int
+	defer func(x *int) {
+		*x = 3
+	}(&y)
+	return a
+}
+
+func f4() (y int) {
+	defer func(x *int) {
+		*x = 4
+	}(&y)
+	return
 }
