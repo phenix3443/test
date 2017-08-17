a34e69d3c469b46b3c62694046060be30f97e8cf
liushangliang <liushangliang@xunlei.com>
2017-08-17 21:55:17 +0800

    add new

--- go/test/src/example.com/hello/hello.go
+++ go/test/src/example.com/hello/hello.go
@@ -2,29 +2,30 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
-func main() {
-	// var i int
-	// fmt.Println(reflect.TypeOf(i), i) // int 0
-	// var f float32
-	// fmt.Println(reflect.TypeOf(f), f) // float32 0
-	// var b byte
-	// fmt.Println(reflect.TypeOf(b), b) // uint8 0
-	// var r rune
-	// fmt.Println(reflect.TypeOf(r), r) // int32 0
-	// var s string
-	// fmt.Println(reflect.TypeOf(s), s) // string
-	// var arr1 [5]int
-	// fmt.Println(reflect.TypeOf(arr1), arr1) // [5]int [0 0 0 0 0]
-	// var slice []int
-	// fmt.Println(reflect.TypeOf(slice), slice) // []int []
-	// var m map[string]int
-	// fmt.Println(reflect.TypeOf(m), m) // map[string]int map[]
-	// var fun = func() {}
-	// fmt.Println(reflect.TypeOf(fun)) // func()
+type Element interface{}
+type semaphore chan Element
 
-	m := new(map[string]string)
-	(*m)["hello"] = "world"
-	fmt.Println(m)
+func (s semaphore) P(n int) {
+	for i := 0; i < n; i++ {
+		s <- new(Element)
+	}
+}
+
+func (s semaphore) V(n int) {
+	for i := 0; i < n; i++ {
+		<-s
+	}
+}
+
+func getData(ch chan string) {
+	time.Sleep(1e9)
+	ch <- "done"
+}
+func main() {
+	ch := make(chan string, 10)
+	go getData(ch)
+	fmt.Println(<-ch)
 }
