b03c05148fad65ad81e6e0413d67a7e45373c298
liushangliang <liushangliang@xunlei.com>
2017-08-29 09:50:41 +0800

    简单修改返回格式

--- lua/utils.lua
+++ lua/utils.lua
@@ -1,4 +1,4 @@
-local string = string
+-- -*- coding:utf-8 -*-
 
 local mystring = {}
 
@@ -20,15 +20,46 @@ mystring.startswith = function(str, start)
 	return string.sub(str,1,string.len(start)) == start
 end
 
-mystring.from_hex =	function (hexstr)
+mystring.hex2bin =	function (hexstr)
 	return (hexstr:gsub('..', function (cc) return string.char(tonumber(cc, 16)) end))
 end
 
-mystring.to_hex = function (binstr)
+mystring.bin2hex = function (binstr)
     return (binstr:gsub('.', function (c) return string.format('%02X', string.byte(c)) end))
 end
 
 
+local mytable  = {}
+
+mytable.shadow_copy = function (t)
+    if type(t) ~= "table" then return t end
+    local meta = getmetatable(t)
+    local target = {}
+    for k, v in pairs(t) do target[k] = v end
+    setmetatable(target, meta)
+    return target
+end
+
+mytable.deep_copy = function (orig)
+	-- http://lua-users.org/wiki/CopyTable
+    local orig_type = type(orig)
+    local copy
+    if orig_type == 'table' then
+        copy = {}
+        for orig_key, orig_value in next, orig, nil do
+            copy[mytable.deep_copy(orig_key)] = mytable.deep_copy(orig_value)
+        end
+        setmetatable(copy, mytable.deep_copy(getmetatable(orig)))
+    else -- number, string, boolean, etc
+        copy = orig
+    end
+
+    return copy
+end
+
+
+
 export = {
-	["string"] = mystring
+	["string"] = mystring,
+	["table"]  = mytable
 }
