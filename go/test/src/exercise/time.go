396b6aeacd5a2ef8661e887a097404c59e099b2e
phenix3443 <phenix3443@gmail.com>
2017-09-27 00:18:50 +0800

    add new

--- go/test/src/exercise/time.go
+++ go/test/src/exercise/time.go
@@ -16,10 +16,21 @@ func timeFormat() {
 		fmt.Printf("%-15s %q gives %q\n", name, layout, got)
 	}
 
+	// time.Time's Stringer method is useful without any format.
+	fmt.Println("default format:", t)
+
+	// Predefined constants in the package implement common layouts.
+	fmt.Println("Unix format:", t.Format(time.UnixDate))
+
+	// The time zone attached to the time value affects its output.
+	fmt.Println("Same, in UTC:", t.UTC().Format(time.UnixDate))
 	// Print a header in our output.
 	fmt.Printf("\nFormats:\n\n")
 
 	// A simple starter example.
 	do("Basic", "Mon Jan 2 15:04:05 MST 2006", "Sat Mar 7 11:06:39 PST 2015")
 	do("No pad", "<2>", "<7>")
+	// An underscore represents a zero pad, if required.
+	do("AM/PM", "2006-01-02 15:04:05", "2015-03-07 11:06:39")
+
 }
