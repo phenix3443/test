7f2736bb066440a05e32102f2023265f2761bb23
phenix3443 <phenix3443@gmail.com>
2017-12-02 21:38:31 +0800

    增加了go语言示例

--- go/test/src/example.com/hello/hello.go
+++ go/test/src/example.com/hello/hello.go
@@ -2,11 +2,21 @@ package main
 
 import (
 	"fmt"
-
-	"example.com/stringutil"
+	"io"
+	"net/http"
 )
 
+// hello world, the web server
+func HelloServer(w http.ResponseWriter, req *http.Request) {
+	io.WriteString(w, "hello, world!\n")
+}
+
 func main() {
-	str := "hello, world"
-	fmt.Println(stringutil.Reverse(str))
+	http.HandleFunc("/hello", HelloServer)
+	err := http.ListenAndServe(":12345", nil)
+	fmt.Printf(":%v:", err)
+	if err != nil {
+		fmt.Println("error")
+		return
+	}
 }
