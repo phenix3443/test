f06a3b95213da692e347e11c5935002ac0bb0448
phenix3443 <phenix3443@gmail.com>
2018-02-07 01:24:55 +0800

    add new

--- docker/network/main.py
+++ docker/network/main.py
@@ -2,10 +2,9 @@ import redis
 
 
 def main():
-    # r = redis.StrictRedis(host="test_redis", db=0)
-    # r.set("foo", "bar")
-    # print(r.get("foo"))
-    print("hello,this is from python container")
+    r = redis.StrictRedis(host="db.app", db=0)
+    r.set("foo", "bar")
+    print(r.get("foo"))
 
 
 if __name__ == "__main__":
