98230d7cb4d74d0ab904df9d7d6fa55f19cc8291
phenix3443 <phenix3443@gmail.com>
2018-03-13 01:03:20 +0800

    add new

---
+++ cpp/src/lang_test/buildin_type_test.cpp
@@ -0,0 +1,26 @@
+// -*- coding:utf-8 -*-
+// author:liushangliang
+// desc: 跳转语句测试
+
+#include "lang_test.hpp"
+
+
+TEST(BuildinTypeTest, AutoConvert) {
+        EXPECT_TRUE(1);
+        EXPECT_FALSE(0);
+
+        EXPECT_EQ(1, true);
+        EXPECT_EQ(0, false);
+
+        int i = 3.24;
+        EXPECT_EQ(3, i);
+
+        int d = 3;
+        EXPECT_DOUBLE_EQ(3.0, d);
+
+        unsigned char uc = 257;
+        EXPECT_EQ(1, uc);
+
+        signed char sc = 128;
+        EXPECT_EQ(1,sc);
+}
