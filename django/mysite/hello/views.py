47c3cafa97e773bcf686f94ddeefa737bc584948
liushangliang <liushangliang@xunlei.com>
2018-05-14 08:52:28 +0800

    add new

--- django/mysite/hello/views.py
+++ django/mysite/hello/views.py
@@ -1,8 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
 
 # Create your views here.
-from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("hello, welcome to django world!")
+    person = {
+        "name": "guest",
+        "message": "welcome",
+    }
+
+    if request.method == "POST":
+        person["name"] = request.POST["name"]
+
+    print(person["name"])
+    context = {"person": person}
+    return render(request, 'hello.html', context)
