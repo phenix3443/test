c44123d3ff181756a3f727bb8bef94d115d76c6c
liushangliang <liushangliang@xunlei.com>
2018-12-12 12:44:47 +0800

    add new

--- lua/test.lua
+++ lua/test.lua
@@ -1,5 +1,3 @@
 -- -*- coding:utf-8 -*-
-
-str = "aaaaa" ..
-    "bbbb"
-print(str)
+package.path = package.path .. ';/usr/local/share/lua/5.1/?.lua'
+package.cpath = package.cpath .. ';/usr/local/share/lua/5.1/?.so'
