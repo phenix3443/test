7a9fce5a5d6b0778b694b17e064e68747c1fecac
phenix3443 <phenix3443@gmail.com>
2019-05-14 22:19:10 +0800

    添加测试字段

--- lua/mmdb/test.lua
+++ lua/mmdb/test.lua
@@ -13,24 +13,25 @@ local function test(ip)
     local city_info = ipdb:get_city_info(ip)
     local isp_info = ipdb:get_isp_info(ip)
 
-    print(sp.block(coun_info))
-    -- print(sp.block(city_info))
-    -- print(sp.block(isp_info))
+    -- print(sp.block(coun_info))
+    print(sp.block(city_info))
+    print(sp.block(isp_info))
 
-    -- print(
-    --     city_info.country.iso_code,
-    --     city_info.country.names["en"],
-    --     city_info.country.names["zh-CN"],
+    print(
+        city_info.country.iso_code,
+        -- city_info.country.names["en"],
+        city_info.country.names["zh-CN"],
 
-    --     (city_info.city and city_info.city.names["zh-CN"]) or "未知",
+        (city_info.subdivisions and city_info.subdivisions[1].names["zh-CN"]) or "未知",
+        (city_info.city and city_info.city.names["zh-CN"]) or "未知",
 
-    --     isp_info.autonomous_system_number,
-    --     isp_info.autonomous_system_organization
-    -- )
+        isp_info.autonomous_system_number,
+        isp_info.autonomous_system_organization
+    )
 end
 
 -- local ip = "213.215.63.11" -- 欧洲 法国 波尔多
 -- local ip = "2607:f8b0:4004:801::100e" --google
-local ip = "13.232.158.182" -- 印度 孟买
--- local ip = "114.119.117.18" -- 中国 广州 联通
+-- local ip = "13.232.158.182" -- 印度 孟买
+local ip = "114.119.117.18" -- 中国 广州 联通
 test(ip)
