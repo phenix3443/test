8b36e8baad9f7a91d6cd40836077e28b15edc384
phenix3443 <phenix3443@gmail.com>
2019-08-06 20:57:00 +0800

    添加bool测试用例

---
+++ c/src/c99/btest.c
@@ -0,0 +1,12 @@
+/* coding:utf-8 */
+#include <stdbool.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    bool cond = true;
+    if (cond) {
+        printf("测试条件为真\n");
+    }
+    return 0;
+}
