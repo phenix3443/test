9da627e180c843f2bf9543c05492119236d22b97
phenix3443 <phenix3443@gmail.com>
2019-08-23 21:28:49 +0800

    add new

--- c/libevent/echo_server.c
+++ c/libevent/echo_server.c
@@ -1,2 +1,13 @@
 /* -*- coding:utf-8; -*- */
+/* 简单的回显服务器 */
+
 #include <stdlib.h>
+#include <event2/event.h>
+#include <sys/socket.h>
+
+
+int main(int argc, char *argv[])
+{
+
+    return 0;
+}
--- c/libevent/evhttp.c
+++ c/libevent/evhttp.c
@@ -1,10 +1,49 @@
 /* -*- coding:utf-8 -*- */
-#include <stdio.h>
-#include <event2/http.h>
+/* libevent实现的简单的http服务器， */
+/* 返回解析以后用户发送的请求内容 */
 
+#include <stdio.h>
+#include <event2/event.h>
+#include <event2/http.h>
+#include <sys/socket.h>
+
+void dump_cb (struct evhttp_request *req, void *data) {
+    struct evbuffer *buff = evbuffer_new();
+}
+
+void hello_cb (struct evhttp_request *req, void *data) {
+
+}
 
 int main(int argc, char *argv[])
 {
+    struct event_base *base = event_base_new();
+    if (NULL==base) {
+        printf("new event base error\n");
+        return -1;
+    }
+    struct evhttp *httpd = evhttp_new(base);
+    if (NULL == httpd) {
+        printf("new http error\n");
+        return -1;
+    }
 
+    /* The /dump URI will dump all requests to stdout and say 200 ok. */
+    evhttp_set_cb(httpd, "/dump", dump_cb, NULL);
+
+    /* We want to accept arbitrary requests, so we need to set a "generic" */
+    /* cb.  We can also add callbacks for specific paths. */
+	evhttp_set_gencb(httpd, hello_cb, NULL);
+
+    struct evhttp_bound_socket *sock = evhttp_bind_socket_with_handle(httpd, "127.0.0.1", 9001);
+    if (!sock) {
+        printf("bind error \n");
+        return -1;
+    }
+
+    event_base_dispatch(base);
+
+    evhttp_free(httpd);
+    event_base_free(base);
     return 0;
 }
