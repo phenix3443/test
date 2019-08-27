5144fcf4d78805c1312c28ad5e9fd35ab9a709fd
phenix3443 <phenix3443@gmail.com>
2019-08-27 18:00:34 +0800

    优化代码

--- c/thread/thread.c
+++ c/thread/thread.c
@@ -1,19 +1,12 @@
 /* -*- coding:utf-8 -*-  */
-/* 线程练习 */
+/* 线程创建练习 */
 
 #include <pthread.h>
 #include <stdio.h>
 #include <unistd.h>
 
-void print_tid () {
-    pid_t pid = getpid ();
-    pthread_t tid = pthread_self ();
-    printf ("pid:%d, tid:%lu\n", pid, tid);
-}
-
 void* work (void *arg) {
-    printf ("%s\n", (char *)arg);
-    print_tid ();
+    printf ("tid:%lu, %s\n", pthread_self(), (char *)arg);
     pthread_exit (NULL);        /* pthread_exit返回值不应该分配在函数栈上 */
 }
 
@@ -24,9 +17,10 @@ int main (int argc, char *argv[]) {
         printf ("thread create error\n");
         return -1;
     }
-    pthread_join (tid, NULL);
 
-    printf("main thread\n");
-    print_tid ();
+    pthread_join (tid, NULL);   /* 等待子线程执行 */
+
+    printf("main thread done\n");
+
     return 0;
 }
