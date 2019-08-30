d2c90965d4e41bc89c7073d83c3c18880b845367
phenix3443 <phenix3443@gmail.com>
2019-08-30 20:07:53 +0800

    添加thpool测试代码

---
+++ c/thpool/test.c
@@ -0,0 +1,28 @@
+/* -*-coding:utf-8;-*- */
+#include <pthread.h>
+#include <stdio.h>
+#include <unistd.h>
+#include "thpool.h"
+#include <stdlib.h>
+void work1 (void *data) {
+    printf ("%lu work 1, count=%d\n", pthread_self (), *(int *)data);
+    usleep (100);
+}
+
+void work2 (void *data) {
+    printf ("%lu work 2, count=%d\n", pthread_self (), *(int *)data);
+    usleep (100);
+}
+
+int main (int argc, char *argv[]) {
+    threadpool tp = thpool_init (2);
+    for (volatile int i = 0; i < 5; ++i) {
+        int *k = (int *)malloc(sizeof(i));
+        thpool_add_work (tp, work1, (void *)&k);
+        thpool_add_work (tp, work2, (void *)&k);
+
+    }
+            thpool_wait (tp);
+    thpool_destroy (tp);
+    return 0;
+}
