2ffdbceab1c1bea11e8abd6152c2489104f52371
phenix3443 <phenix3443@gmail.com>
2019-09-04 16:16:31 +0800

    格式化代码

--- c/thpool/test.c
+++ c/thpool/test.c
@@ -1,9 +1,9 @@
 /* -*-coding:utf-8;-*- */
 #include <pthread.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include "thpool.h"
-#include <stdlib.h>
 void work1 (void *data) {
     printf ("%lu work 1, count=%d\n", pthread_self (), *(int *)data);
     usleep (100);
@@ -17,12 +17,11 @@ void work2 (void *data) {
 int main (int argc, char *argv[]) {
     threadpool tp = thpool_init (2);
     for (volatile int i = 0; i < 5; ++i) {
-        int *k = (int *)malloc(sizeof(i));
+        int *k = (int *)malloc (sizeof (i));
         thpool_add_work (tp, work1, (void *)&k);
         thpool_add_work (tp, work2, (void *)&k);
-
     }
-            thpool_wait (tp);
+    thpool_wait (tp);
     thpool_destroy (tp);
     return 0;
 }
