f58b15cd41a4d2c06f3a2d7357f9fe69b9a2a010
phenix3443 <phenix3443@gmail.com>
2019-09-29 17:08:54 +0800

    添加doxygen注释

--- c/cmockery/overview.c
+++ c/cmockery/overview.c
@@ -1,5 +1,5 @@
 /* -*- coding:utf-8; -*- */
-/* 测试cmockery测试代码架构 */
+
 #include <assert.h>
 #include <stdbool.h>
 
@@ -9,11 +9,22 @@
 
 #include <google/cmockery.h> /* 注意该头文件依赖上面三个头文件，注意顺序 */
 
+/**
+ * @fn  empty_test
+ * 
+ * @brief      function description
+ *
+ * @details    detailed description
+ *
+ * @param      param
+ *
+ * @return     return type
+ */
 void empty_test (void **state) {}
 
 int main (int argc, char *argv[]) {
     const UnitTest tests[] = {
-        unit_test (empty_test),
+        unit_test (empty_test), /*!< 空测试 */
     };
     return run_tests (tests);
 }
