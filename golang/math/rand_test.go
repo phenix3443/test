7da0522ddd060c72762e44ee1c8aedf230146fc2
liushangliang <liushangliang@xunlei.com>
2019-11-07 22:04:32 +0800

    增加测试用例

--- golang/math/rand_test.go
+++ golang/math/rand_test.go
@@ -3,7 +3,9 @@ package rand_test
 import (
 	"fmt"
 	"math/rand"
+	"strings"
 	"testing"
+	"time"
 
 	check "gopkg.in/check.v1"
 )
@@ -16,13 +18,36 @@ type RandSuit struct{}
 
 var _ = check.Suite(&RandSuit{})
 
-func (s *RandSuit) testRand(t *testing.T) {
-	for i := 1; i < 10; i++ {
-		fmt.Println(rand.Intn(10))
-	}
+func (s *RandSuit) TestInt(c *check.C) {
+	fmt.Println(rand.Int())
+	fmt.Println(rand.Int())
+	fmt.Println(rand.Int())
 }
 
-func (s *RandSuit) testPerm(t *testing.T) {
+func (s *RandSuit) TestIntn(c *check.C) {
+	fmt.Println(rand.Intn(10))
+	fmt.Println(rand.Intn(10))
+	fmt.Println(rand.Intn(10))
+}
+
+func (s *RandSuit) TestPerm(c *check.C) {
 	arr := rand.Perm(5)
+	c.Assert(arr, check.HasLen, 5)
 	fmt.Println(arr)
+}
+
+func (s *RandSuit) TestSeed(c *check.C) {
+	rand.Seed(time.Now().UTC().UnixNano())
+	fmt.Println(rand.Intn(10))
+	fmt.Println(rand.Intn(10))
+	fmt.Println(rand.Intn(10))
+}
+
+func (s *RandSuit) TestShuffle(c *check.C) {
+	rand.Seed(time.Now().UTC().UnixNano())
+	words := strings.Fields("ink runs from the corners of my mouth")
+	rand.Shuffle(len(words), func(i, j int) {
+		words[i], words[j] = words[j], words[i]
+	})
+	fmt.Println(words)
 }
