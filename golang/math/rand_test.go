d4f463c315361ef2d47b9be7901f00578559df4d
phenix3443 <phenix3443@gmail.com>
2019-11-07 20:19:03 +0800

    add new

--- golang/math/rand_test.go
+++ golang/math/rand_test.go
@@ -1,11 +1,11 @@
-package my_rand_test
+package rand_test
 
 import (
 	"fmt"
 	"math/rand"
 	"testing"
 
-	"gopkg.in/check.v1"
+	check "gopkg.in/check.v1"
 )
 
 func Test(t *testing.T) {
@@ -16,13 +16,13 @@ type RandSuit struct{}
 
 var _ = check.Suite(&RandSuit{})
 
-func (s *RandSuit) TestRand(c *check.C) {
+func (s *RandSuit) testRand(t *testing.T) {
 	for i := 1; i < 10; i++ {
 		fmt.Println(rand.Intn(10))
 	}
 }
 
-func (s *RandSuit) TestPerm(c *check.C) {
+func (s *RandSuit) testPerm(t *testing.T) {
 	arr := rand.Perm(5)
 	fmt.Println(arr)
 }
