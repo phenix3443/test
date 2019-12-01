43d5c0bef56754d5f42d9c5628e44bd0ae47f084
phenix3443 <phenix3443@gmail.com>
2019-12-02 00:34:38 +0800

    add new

--- lua/test.lua
+++ lua/test.lua
@@ -3,13 +3,21 @@ package.path = package.path .. ';/usr/local/share/lua/5.1/?.lua'
 package.cpath = package.cpath .. ';/usr/local/share/lua/5.1/?.so'
 
 
-local os = require("os")
+
 local string = require("string")
 
 t = {a="a",b="b"}
 
 
-function f ()
+function display ()
     print(os.time())
     print(t.a)
+end
+
+
+
+
+
+function main ()
+	display()
 end
