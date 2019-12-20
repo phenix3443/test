02c154ca5cb0a656484d88070b260fd67bcd219a
liushangliang <liushangliang@xunlei.com>
2019-12-20 14:13:49 +0800

    add new

--- golang/collections/set/main.go
+++ golang/collections/set/main.go
@@ -6,9 +6,13 @@ import (
 	"github.com/golang-collections/collections/set"
 )
 
+func wrapFunc(e interface{}) { fmt.Println(e) }
+
 func main() {
 	s := set.New(1, 2, 3, 4)
 	if s.Has(3) {
-		fmt.Println("has")
+		fmt.Println("has 3 in set")
 	}
+	// 测试do函数
+	s.Do(wrapFunc)
 }
