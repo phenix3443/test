d6dbbe551de6bb39d4e9dffdbcbdfc7a0f9ee34d
phenix3443 <phenix3443@gmail.com>
2019-12-29 23:33:50 +0800

    add new

--- golang/zap/main.go
+++ golang/zap/main.go
@@ -1,21 +1,79 @@
 package main
 
 import (
-	"time"
+	"encoding/json"
 
 	"go.uber.org/zap"
 )
 
-func zapSugar() {
-	logger, _ := zap.NewProduction()
-	url := "www.baidu.com"
-	defer logger.Sync() // flushes buffer, if any
-	sugar := logger.Sugar()
-	sugar.Infow("failed to fetch URL",
-		// Structured context as loosely typed key-value pairs.
-		"url", url,
-		"attempt", 3,
-		"backoff", time.Second,
-	)
-	sugar.Infof("Failed to fetch URL: %s", url)
+func zapSugared() {
+	logger := zap.NewExample().Sugar()
+	defer logger.Sync()
+
+	logger.Debug("debug message with println foramat")
+	logger.Debugf("debug message with printf format:%s", "foo")
+	logger.Debugw("debug message with k,v", "foo", "bar")
+}
+
+func zapLogger() {
+
+}
+
+func childLogger() {
+	logger := zap.NewExample().Sugar()
+	defer logger.Sync()
+
+	logger.Debug("main logger")
+
+	cLogger := logger.With("child", true)
+	cLogger.Debug("child logger")
+}
+
+func basicConfig() {
+	var cfg zap.Config
+
+	// cfg.Level.SetLevel(zap.InfoLevel)
+	// cfg.OutputPaths = []string{"/tmp/stdout"}
+	// cfg.ErrorOutputPaths = []string{"/tmp/stderr"}
+	// cfg.Encoding = "json"
+	// cfg.EncoderConfig.MessageKey = "msg"
+	// cfg.EncoderConfig.LevelKey = "level"
+	// cfg.InitialFields = map[string]interface{}{"foo": "bar"}
+
+	rawJSON := []byte(`{
+		"level": "debug",
+		"outputPaths": ["/tmp/stdout"],
+		"errorOutputPaths": ["/tmp/stderr"],
+		"encoding": "json",
+		"encoderConfig": {
+			"messageKey": "message",
+			"levelKey": "level",
+			"levelEncoder": "lowercase"
+		}
+	}`)
+
+	if err := json.Unmarshal(rawJSON, &cfg); err != nil {
+		panic(err)
+	}
+
+	logger, err := cfg.Build()
+	if err != nil {
+		panic(err)
+	}
+	defer logger.Sync()
+
+	logger.Sugar().Debug("debug log show not diplay")
+
+	logger.Sugar().Error("info log display")
+}
+
+func advanceConfig() {
+
+}
+
+func main() {
+	// zapSugared()
+
+	// childLogger()
+	basicConfig()
 }
