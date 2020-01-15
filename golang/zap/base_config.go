13fad568522ce4a32e58ef4f83101dac3b5f94c0
liushangliang <liushangliang@xunlei.com>
2020-01-15 21:55:16 +0800

    add new

--- golang/zap/base_config.go
+++ golang/zap/base_config.go
@@ -1,50 +1,39 @@
 package main
 
 import (
-	"encoding/json"
-
 	"go.uber.org/zap"
 )
 
 func basicConfig() {
-	rawJSON := []byte(`{
-		"level": "debug",
-		"encoding": "json",
-		"outputPaths": ["stdout","/tmp/stdout"],
-		"errorOutputPaths": ["stdout","/tmp/stderr"],
-        "initialFields":{"foo":"bar"},
-		"encoderConfig": {
-			"messageKey": "message",
-			"levelKey": "level",
-			"levelEncoder": "lowercase"
-		}
-	}`)
+	// encoderConfig := zap.NewDevelopmentEncoderConfig()
 
-	var cfg zap.Config
-	if err := json.Unmarshal(rawJSON, &cfg); err != nil {
-		panic(err)
+	encoderConfig := zap.NewProductionEncoderConfig()
+
+	// encoderConfig := zapcore.EncoderConfig{
+	// 	MessageKey: "msg",
+	// 	LevelKey:   "level",
+	// 	TimeKey:    "time",
+	// 	NameKey:    "logger",
+	// 	// CallerKey:      "caller",
+	// 	// StacktraceKey:  "stacktrace",
+	// 	LineEnding:     zapcore.DefaultLineEnding,
+	// 	EncodeLevel:    zapcore.LowercaseLevelEncoder, // 小写编码器
+	// 	EncodeTime:     zapcore.ISO8601TimeEncoder,    // ISO8601 UTC 时间格式
+	// 	EncodeDuration: zapcore.SecondsDurationEncoder,
+	// 	EncodeCaller:   zapcore.FullCallerEncoder, // 全路径编码器
+	// }
+	atomLevel := zap.NewAtomicLevelAt(zap.DebugLevel)
+	cfg := zap.Config{
+		Level:             atomLevel,
+		Development:       false,
+		DisableCaller:     true,
+		DisableStacktrace: true,
+		Encoding:          "json",
+		EncoderConfig:     encoderConfig,
+		OutputPaths:       []string{"stdout", "/tmp/stdout"},
+		ErrorOutputPaths:  []string{"stdout", "/tmp/zaperr"},
+		InitialFields:     map[string]interface{}{"foo": "bar"},
 	}
-
-	logger, err := cfg.Build()
-	if err != nil {
-		panic(err)
-	}
-	defer logger.Sync()
-
-	logger.Sugar().Debug("base configure debug log")
-
-}
-
-func basicConfig2() {
-	var cfg zap.Config
-
-	cfg.Level.SetLevel(zap.InfoLevel)
-	cfg.OutputPaths = []string{"/tmp/stdout"}
-	cfg.ErrorOutputPaths = []string{"/tmp/stderr"}
-	cfg.Encoding = "json"
-	cfg.EncoderConfig.MessageKey = "msg"
-	cfg.EncoderConfig.LevelKey = "level"
-	cfg.InitialFields = map[string]interface{}{"foo": "bar"}
 
 	logger, err := cfg.Build()
 	if err != nil {
